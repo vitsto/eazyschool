7c7f73eed5ffbc880890a8531d3ed64bdfc1601e
vitsto <stolyarov.vitalij@list.ru>
2023-09-29 16:33:10 +1000

    Submit information from Contact page using @RequestParam

---
+++ src/main/java/com/eazybytes/eazyschool/controller/CourseController.java
@@ -0,0 +1,12 @@
+package com.eazybytes.eazyschool.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class CourseController {
+//    @RequestMapping("/courses")
+//    public String displayCourses() {
+//        return "courses.html";
+//    }
+}
