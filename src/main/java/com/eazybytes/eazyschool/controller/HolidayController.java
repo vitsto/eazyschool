9f1dea13a82faa82791575d0533428e90a93ab9d
vitsto <stolyarov.vitalij@list.ru>
2023-10-03 16:16:31 +1000

    Adding Bean Validation annotations inside Contact POJO class

--- src/main/java/com/eazybytes/eazyschool/controller/HolidayController.java
+++ src/main/java/com/eazybytes/eazyschool/controller/HolidayController.java
@@ -4,6 +4,8 @@ import com.eazybytes.eazyschool.model.Holiday;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestParam;
 
 import java.util.Arrays;
 import java.util.List;
@@ -12,17 +14,27 @@ import java.util.stream.Collectors;
 @Controller
 public class HolidayController {
 
-    @GetMapping("/holidays")
-    public String displayHolidays(Model model) {
+    @GetMapping("/holidays/{display}")
+    public String displayHolidays(@PathVariable String display,
+                                  Model model) {
+        if (null != display && display.equals("all")) {
+            model.addAttribute("festival", true);
+            model.addAttribute("federal", true);
+        } else if (null != display && display.equals("federal")) {
+            model.addAttribute("federal", true);
+        } else if (null != display && display.equals("festival")) {
+            model.addAttribute("festival", true);
+        }
+
         List<Holiday> holidays = Arrays.asList(
-                new Holiday(" Jan 1 ","New Year's Day", Holiday.Type.FESTIVAL),
-                new Holiday(" Oct 31 ","Halloween", Holiday.Type.FESTIVAL),
-                new Holiday(" Nov 24 ","Thanksgiving Day", Holiday.Type.FESTIVAL),
-                new Holiday(" Dec 25 ","Christmas", Holiday.Type.FESTIVAL),
-                new Holiday(" Jan 17 ","Martin Luther King Jr. Day", Holiday.Type.FEDERAL),
-                new Holiday(" July 4 ","Independence Day", Holiday.Type.FEDERAL),
-                new Holiday(" Sep 5 ","Labor Day", Holiday.Type.FEDERAL),
-                new Holiday(" Nov 11 ","Veterans Day", Holiday.Type.FEDERAL)
+                new Holiday(" Jan 1 ", "New Year's Day", Holiday.Type.FESTIVAL),
+                new Holiday(" Oct 31 ", "Halloween", Holiday.Type.FESTIVAL),
+                new Holiday(" Nov 24 ", "Thanksgiving Day", Holiday.Type.FESTIVAL),
+                new Holiday(" Dec 25 ", "Christmas", Holiday.Type.FESTIVAL),
+                new Holiday(" Jan 17 ", "Martin Luther King Jr. Day", Holiday.Type.FEDERAL),
+                new Holiday(" July 4 ", "Independence Day", Holiday.Type.FEDERAL),
+                new Holiday(" Sep 5 ", "Labor Day", Holiday.Type.FEDERAL),
+                new Holiday(" Nov 11 ", "Veterans Day", Holiday.Type.FEDERAL)
         );
         Holiday.Type[] types = Holiday.Type.values();
         for (Holiday.Type type : types) {
