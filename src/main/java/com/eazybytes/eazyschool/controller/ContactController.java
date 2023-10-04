fc8e2634e2f74fbccb283008a46c29d0f96ca858
vitsto <stolyarov.vitalij@list.ru>
2023-10-04 16:15:15 +1000

    Use Cases of Spring Web Scopes

--- src/main/java/com/eazybytes/eazyschool/controller/ContactController.java
+++ src/main/java/com/eazybytes/eazyschool/controller/ContactController.java
@@ -47,6 +47,8 @@ public class ContactController {
             return "contact.html";
         }
         contactService.saveMessageDetails(contact);
+        contactService.setCounter(contactService.getCounter() + 1);
+        log.info("Number of times the Contact form is submitted : " + contactService.getCounter());
         return "redirect:/contact";
     }
 }
--- src/main/java/com/eazybytes/eazyschool/service/ContactService.java
+++ src/main/java/com/eazybytes/eazyschool/service/ContactService.java
@@ -6,15 +6,34 @@ import lombok.extern.slf4j.Slf4j;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.stereotype.Service;
+import org.springframework.web.context.annotation.ApplicationScope;
+import org.springframework.web.context.annotation.RequestScope;
+import org.springframework.web.context.annotation.SessionScope;
 
 @Slf4j
 @Service
+//@RequestScope
+//@SessionScope
+//@ApplicationScope
 public class ContactService {
+    private int counter = 0;
+
+    public ContactService() {
+        System.out.println("Contact Service Bean initialized");
+    }
 
     public boolean saveMessageDetails(Contact contact) {
         boolean isSaved = true;
 
         log.info(contact.toString());
         return isSaved;
+    }
+
+    public int getCounter() {
+        return counter;
+    }
+
+    public void setCounter(int counter) {
+        this.counter = counter;
     }
 }
