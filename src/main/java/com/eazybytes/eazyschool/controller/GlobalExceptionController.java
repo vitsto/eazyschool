33ba86b5ed452186ad9b8cc02ef1020c8429583f
vitsto <vitsto94@gmail.com>
2023-10-12 00:16:42 +1000

    login and logout

---
+++ src/main/java/com/eazybytes/eazyschool/controller/GlobalExceptionController.java
@@ -0,0 +1,18 @@
+package com.eazybytes.eazyschool.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.ControllerAdvice;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.servlet.ModelAndView;
+
+@ControllerAdvice
+public class GlobalExceptionController {
+
+    @ExceptionHandler(Exception.class)
+    public ModelAndView exceptionHandler(Exception exception) {
+        ModelAndView errorPage = new ModelAndView();
+        errorPage.setViewName("error");
+        errorPage.addObject("errormsg", exception.getMessage());
+        return errorPage;
+    }
+}
