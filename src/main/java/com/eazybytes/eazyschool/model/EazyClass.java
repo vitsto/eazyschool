827bc0f6ec12a44d9b8dcba7c1cd8a4fac645878
vitsto <vitsto94@gmail.com>
2024-01-09 08:29:25 +1000

    Display, Add & Delete Students enhancement inside Eazy School Web App - Part 2

--- src/main/java/com/eazybytes/eazyschool/model/EazyClass.java
+++ src/main/java/com/eazybytes/eazyschool/model/EazyClass.java
@@ -4,16 +4,19 @@ import jakarta.persistence.*;
 import jakarta.validation.constraints.NotBlank;
 import jakarta.validation.constraints.Size;
 import lombok.Data;
+import lombok.Getter;
+import lombok.Setter;
 
 import java.util.Set;
 
-@Data
+@Getter
+@Setter
 @Entity
 @Table(name = "class")
 public class EazyClass extends BaseEntity {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
-    private Long id;
+    private Long classId;
 
     @NotBlank(message = "Name must not be blank")
     @Size(min = 3, message = "Name must be at least 3 characters long")
